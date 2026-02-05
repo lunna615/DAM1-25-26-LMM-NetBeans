66cbea0e1b8a3dbd28535577f2f6b70f895dc127
Lunna <Lunna@DESKTOP-4C4CHU3>
2026-02-05 12:40:58 +0100

    Primer commit

---
+++ src/dam1/pkg25/pkg26/lmm/netbeans/DAM12526LMMNetBeans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dam1.pkg25.pkg26.lmm.netbeans;
+
+/**
+ *
+ * @author Lunna
+ */
+public class DAM12526LMMNetBeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
